75ebf649e3b593d0c19a005ad66774b4591dba1d
mpgxc <mpgx5.c@gmail.com>
2020-03-27 19:16:43 -0300

    feat: Mobile > Initial commit

---
+++ Mobile/src/pages/Incidents/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { View, Text, Image, StyleSheet } from "react-native";
+import Logo from "../../assets/logo.png";
+
+export default () => {
+  return (
+    <View styles={styles.container}>
+      <View style={styles.header}>
+        <Image source={Logo} />
+        <Text style={styles.headerText}>
+          Total de <Text style={styles.headerTextBold}>0 casos</Text>
+        </Text>
+      </View>
+      <Text style={styles.title}>Bem-Vindo!</Text>
+      <Text style={styles.description}>Exemplo de Descrição</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {}
+});

393686aa9178c325dba2499cfa155f8e069cf59b
mpgxc <mpgx5.c@gmail.com>
2020-03-29 06:01:46 -0300

    add: mobile screens

--- Mobile/src/pages/Detail.js
+++ Mobile/src/pages/Detail.js
@@ -1,6 +1,21 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import Constants from "expo-constants";
 
 export default () => {
-  return <View />;
+  return (
+    <View style={styles.container}>
+      <Text>Olá, mundo!</Text>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 24,
+    paddingTop: Constants.statusBarHeight + 20
+  }
+});
--- Mobile/src/pages/Incidents.js
+++ Mobile/src/pages/Incidents.js
@@ -1,18 +1,65 @@
 import React from "react";
-import { View, Text, Image, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  Image,
+  StyleSheet,
+  TouchableOpacity
+} from "react-native";
+import { Feather } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import Constants from "expo-constants";
+
 import Logo from "../assets/logo.png";
 
 export default () => {
+  const navigation = useNavigation();
+
+  function navigateToDetail() {
+    navigation.navigate("Details");
+  }
+
   return (
-    <View styles={styles.container}>
+    <View style={styles.container}>
       <View style={styles.header}>
         <Image source={Logo} />
         <Text style={styles.headerText}>
           Total de <Text style={styles.headerTextBold}>0 casos</Text>
         </Text>
       </View>
+
       <Text style={styles.title}>Bem-Vindo!</Text>
-      <Text style={styles.description}>Exemplo de Descrição</Text>
+      <Text style={styles.description}>
+        Exemplo de Descrição simplementes para teste. opa olha s'que legal hein
+      </Text>
+
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        style={styles.incidentList}
+        keyExtractor={item => String(item)}
+        data={[1, 2, 3, 4, 5]}
+        renderItem={() => (
+          <View style={styles.incident}>
+            <Text style={styles.incidentProperty}>ONG</Text>
+            <Text style={styles.incidentValue}> APAD</Text>
+
+            <Text style={styles.incidentProperty}>CASO</Text>
+            <Text style={styles.incidentValue}> APAD</Text>
+
+            <Text style={styles.incidentProperty}>ONG</Text>
+            <Text style={styles.incidentValue}> 120,67</Text>
+
+            <TouchableOpacity
+              style={styles.detailsButton}
+              onPress={navigateToDetail}
+            >
+              <Text style={styles.detailsButtonText}>Ver Mais</Text>
+              <Feather name="arrow-right" size={16} color="#E02041" />
+            </TouchableOpacity>
+          </View>
+        )}
+      />
     </View>
   );
 };
@@ -20,6 +67,72 @@ export default () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingHorizontal: 2
+    paddingHorizontal: 24,
+    paddingTop: Constants.statusBarHeight + 20
+  },
+
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+
+  headerText: {
+    fontSize: 15,
+    color: "#737380"
+  },
+
+  headerTextBold: {
+    fontWeight: "bold"
+  },
+
+  title: {
+    fontSize: 30,
+    marginBottom: 16,
+    marginTop: 48,
+    color: "#13131a",
+    fontWeight: "bold"
+  },
+
+  description: {
+    fontSize: 16,
+    lineHeight: 24,
+    color: "#737380"
+  },
+
+  incidentList: {
+    marginTop: 30
+  },
+
+  incident: {
+    padding: 24,
+    borderRadius: 8,
+    backgroundColor: "#FFF",
+    marginBottom: 16
+  },
+
+  incidentProperty: {
+    fontSize: 14,
+    color: "#41414d",
+    fontWeight: "bold"
+  },
+
+  incidentValue: {
+    marginTop: 8,
+    fontSize: 14,
+    marginBottom: 24,
+    color: "#737380"
+  },
+
+  detailsButton: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+
+  detailsButtonText: {
+    color: "#e02041",
+    fontSize: 15,
+    fontWeight: "bold"
   }
 });
